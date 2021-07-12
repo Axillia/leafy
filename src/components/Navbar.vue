1be0e794585e0669f0cc273f2f14a22619cd32ee
Kushan Shamika <shamikakushan@gmail.com>
2021-07-12 21:02:54 +0530

    Home Page

---
+++ src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav>
+    <v-toolbar flat>
+      <div class="d-flex align-center">
+        <v-img
+            alt="Vuetify Logo"
+            class="shrink mr-2"
+            contain
+            :src="require('@/assets/logo.png')"
+            transition="scale-transition"
+            width="150"
+        />
+      </div>
+      <v-spacer></v-spacer>
+      <v-btn color="warning">
+        <v-icon left>mdi-account</v-icon>
+        <span>Profile</span>
+      </v-btn>
+    </v-toolbar>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,150 @@
 <template>
-  <hello-world />
+  <v-container>
+    <v-row no-gutters>
+      <v-col
+          cols="12"
+          sm="6"
+      >
+        <v-img
+        :src="require('@/assets/bg.jpg')"></v-img>
+      </v-col>
+      <v-col
+          cols="12"
+          sm="6"
+      >
+        <v-container fill-height>
+          <v-layout align-center justify-center row>
+            <v-flex class="text-h3 text-center font-weight-bold" xs12 sm12 md12>Welcome to Leafy</v-flex>
+            <v-flex class="text-h5 text-center font-weight-bold" xs12 sm12 md12>Live green to save more green</v-flex>
+            <v-flex class="text-h5 text-center font-weight-bold" xs12 sm12 md12>
+              <v-chip
+                  class="ma-2"
+                  color="green"
+                  text-color="white"
+                  label
+                  large
+              >
+                <v-icon left>
+                  mdi-book-open-page-variant
+                </v-icon>
+                Books
+              </v-chip>
+              <v-chip
+                  class="ma-2"
+                  color="orange"
+                  text-color="white"
+                  label
+                  large
+              >
+                <v-icon left>
+                  mdi-cellphone
+                </v-icon>
+                Mobile Phones
+              </v-chip>
+              <v-chip
+                  class="ma-2"
+                  color="primary"
+                  text-color="white"
+                  label
+                  large
+              >
+                <v-icon left>
+                  mdi-laptop
+                </v-icon>
+                Computers
+              </v-chip>
+              <v-chip
+                  class="ma-2"
+                  color="teal"
+                  text-color="white"
+                  label
+                  large
+              >
+                <v-icon left>
+                  mdi-google-controller
+                </v-icon>
+                Electronics
+              </v-chip>
+              <v-chip
+                  class="ma-2"
+                  color="cyan"
+                  text-color="white"
+                  label
+                  large
+              >
+                <v-icon left>
+                  mdi-seat
+                </v-icon>
+                Furniture
+              </v-chip>
+              <v-chip
+                  class="ma-2"
+                  color="deep-purple accent-4"
+                  text-color="white"
+                  label
+                  large
+              >
+                <v-icon left>
+                  mdi-hammer-screwdriver
+                </v-icon>
+                Constructions
+              </v-chip>
+              <v-chip
+                  class="ma-2"
+                  color="red"
+                  text-color="white"
+                  label
+                  large
+              >
+                <v-icon left>
+                  mdi-wrench
+                </v-icon>
+                Services
+              </v-chip>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-col>
+    </v-row>
+    <v-row no-gutters class="ma-16">
+      <v-container>
+        <v-layout align-center justify-center row>
+          <v-flex class="text-h4 text-center font-weight-bold" xs12 sm12 md12>Take a stand for the love of green</v-flex>
+          <v-btn
+              color="primary"
+              dark
+              large
+              class="mt-16 ma-5"
+          >
+            <v-icon left>
+              mdi-magnify
+            </v-icon>
+            Find your need
+          </v-btn>
+          <v-btn
+              color="orange"
+              dark
+              large
+              class="mt-16 ma-5"
+          >
+            <v-icon left>
+              mdi-rocket
+            </v-icon>
+            Donate your goods
+          </v-btn>
+        </v-layout>
+      </v-container>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
 
   export default {
     name: 'Home',
 
     components: {
-      HelloWorld,
+
     },
   }
 </script>
