23ab088e629ea6c3c4f53d97dcf60dd16b2839f3
Kushan Shamika <shamikakushan@gmail.com>
2021-07-31 12:56:56 +0530

    login

--- src/router/index.js
+++ src/router/index.js
@@ -24,6 +24,11 @@ const routes = [
     path: '/profile',
     name: 'Profile',
     component: () => import('../views/Profile')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/Login')
   }
 ]
 
---
+++ src/views/Login.vue
@@ -0,0 +1,74 @@
+<template>
+<v-container>
+  <v-layout row wrap>
+    <v-flex xs12 md8>
+      <v-img
+          :src="require('@/assets/login.jpg')"
+      ></v-img>
+    </v-flex>
+    <v-flex xs12 md4>
+      <v-container fill-height>
+        <v-layout align-center justify-center row>
+          <v-flex class="text-h3 text-center" xs12 sm12 md12>Welcome Back :)</v-flex>
+          <v-flex xs12 sm12 md12>
+            <v-row
+                class="ma-2"
+            >
+              <v-col
+                  cols="12"
+              >
+                <v-text-field
+                    label="Email Address"
+                    prepend-inner-icon="mdi-email"
+                    outlined
+                ></v-text-field>
+                <v-text-field
+                    label="Password"
+                    prepend-inner-icon="mdi-lock"
+                    outlined
+                ></v-text-field>
+                <v-btn text>
+                  Forget Password?
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-flex>
+          <v-flex xs12 sm12 md12>
+            <v-row
+                class="ma-2"
+            >
+              <v-col>
+                <v-btn
+                    class="mr-2"
+                    depressed
+                    large
+                    color="primary"
+                >
+                  Login Now
+                </v-btn>
+                <v-btn
+                    class="ml-2"
+                    depressed
+                    large
+                >
+                  Create Account
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-flex>
+  </v-layout>
+</v-container>
+</template>
+
+<script>
+export default {
+  name: "Login"
+}
+</script>
+
+<style scoped>
+
+</style>
