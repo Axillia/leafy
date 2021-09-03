b85cab585dc7b8fb18200083b7175c3f32f191d0
Kushan Shamika <shamikakushan@gmail.com>
2021-09-04 00:18:47 +0530

    tawk integration

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 import vUploader from 'v-uploader';
+import Tawk from 'vue-tawk'
 
 const uploaderConfig = {
   uploadFileUrl: `${process.env.VUE_APP_BASE_URL}/upload`,
@@ -15,6 +16,9 @@ const uploaderConfig = {
 
 Vue.config.productionTip = false
 Vue.use(vUploader, uploaderConfig)
+Vue.use(Tawk, {
+  tawkSrc: 'https://embed.tawk.to/61326407649e0a0a5cd47de4/1femddgfv'
+})
 
 new Vue({
   router,
